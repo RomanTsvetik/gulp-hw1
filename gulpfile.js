3e70d7d1d51667324b81edb9947a4742373e1c9c
RomanTsvetik <85985444+RomanTsvetik@users.noreply.github.com>
2021-11-25 13:40:48 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,49 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+const { src, dest, parallel, series, watch } = require("gulp");
+const browserSync = require("browser-sync").create();
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const sass = require("gulp-sass")(require("sass"));
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+
+function browsersync() {
+    browserSync.init({
+        server: { baseDir: "dist/" },
+        notify: false,
+        online: true,
+    });
 }
 
-exports.default = defaultTask;
+function scripts() {
+    return src(["dist/js/script.js"])
+        .pipe(concat("dist.min.js"))
+        .pipe(uglify())
+        .pipe(dest("dist/js/"))
+        .pipe(browserSync.stream());
+}
+
+function styles() {
+    return src("dist/scss/**/*.scss")
+        .pipe(sass())
+        .pipe(concat("dist/min.css"))
+        .pipe(autoprefixer({ overrideBrowserslist: ["last 3 versions"] }))
+        .pipe(
+            cleanCSS({
+                level: { 1: { specialComments: 0 } },
+                // format: "beautify",
+            })
+        )
+        .pipe(dest("dist/css/"))
+        .pipe(browserSync.stream());
+}
+
+function startWatch() {
+    watch('dist/**/*.scss', styles);
+    watch(["dist/**/*.js", "!dist/**/*.min.js"], scripts);
+    watch('dist/**/*.html').on('change', browserSync.reload)
+}
+
+exports.browsersync = browsersync;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.default = parallel(scripts, styles, browsersync, startWatch)
